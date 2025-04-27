6ce7776db5fd1cdfe3bdc9cef7603d6d3524044f
ManApart <r@austinkucera.com>
2025-04-27 15:55:44 -0400

    col has

--- src/commonMain/kotlin/Puzzle.kt
+++ src/commonMain/kotlin/Puzzle.kt
@@ -2,12 +2,13 @@ private val puzzleWidth = (0 until 9)
 
 class Puzzle {
     private val grid = puzzleWidth.associateWith { puzzleWidth.map { Cell() }.toTypedArray() }
-    fun get(x: Int, y: Int) = grid[y]?.let { it[x].value }
-    fun set(x: Int, y: Int, value: Int) {
+    operator fun get(x: Int, y: Int) = grid[y]?.let { it[x].value }
+    operator fun set(x: Int, y: Int, value: Int) {
         grid[y]?.let { it[x] = Cell(value) }
     }
 
     fun rowHas(row: Int, value: Int) = puzzleWidth.any { get(it, row) == value }
+    fun colHas(col: Int, value: Int) = puzzleWidth.any { get(col, it) == value }
 }
 
 data class Cell(var value: Int? = null)
--- src/jvmTest/kotlin/GridTest.kt
+++ src/jvmTest/kotlin/GridTest.kt
@@ -8,15 +8,23 @@ class GridTest {
     @Test
     fun basicAccess(){
         val puzzle = Puzzle()
-        puzzle.set(0,0, 5)
-        assertEquals(5, puzzle.get(0,0))
+        puzzle[0, 0] = 5
+        assertEquals(5, puzzle[0, 0])
     }
 
     @Test
     fun rowHas(){
         val puzzle = Puzzle()
-        puzzle.set(0,0, 5)
-        assertTrue(puzzle.rowHas(0,5))
+        puzzle[1, 2] = 5
+        assertTrue(puzzle.rowHas(2,5))
         assertFalse(puzzle.rowHas(1,5))
+    }
+
+    @Test
+    fun colHas(){
+        val puzzle = Puzzle()
+        puzzle[3, 4] = 5
+        assertTrue(puzzle.colHas(3,5))
+        assertFalse(puzzle.colHas(4,5))
     }
 }
