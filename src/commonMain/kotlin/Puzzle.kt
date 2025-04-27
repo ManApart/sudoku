28643ea181c36cb8daf700ab08495374b919aaeb
ManApart <r@austinkucera.com>
2025-04-27 15:47:42 -0400

    basic access

--- src/commonMain/kotlin/Puzzle.kt
+++ src/commonMain/kotlin/Puzzle.kt
@@ -1,3 +1,9 @@
 class Puzzle {
-    fun get(x: Int, y: Int) = 0
+    private val grid = (0 until 9).associateWith { (0 until 9).map { Cell() }.toTypedArray() }
+    fun get(x: Int, y: Int) = grid[y]?.let { it[x].value }
+    fun set(x: Int, y: Int, value: Int) {
+        grid[y]?.let { it[x] = Cell(value) }
+    }
 }
+
+data class Cell(var value: Int? = null)
--- src/jvmTest/kotlin/GridTest.kt
+++ src/jvmTest/kotlin/GridTest.kt
@@ -4,7 +4,9 @@ import kotlin.test.assertEquals
 class GridTest {
 
     @Test
-    fun helloWorld(){
-        assertEquals("true", "true")
+    fun basicAccess(){
+        val puzzle = Puzzle()
+        puzzle.set(0,0, 5)
+        assertEquals(5, puzzle.get(0,0))
     }
 }
