db846b0b5373efc2b9254d6265f51024d2c5cce0
ManApart <r@austinkucera.com>
2025-04-27 15:51:27 -0400

    row has

--- src/commonMain/kotlin/Puzzle.kt
+++ src/commonMain/kotlin/Puzzle.kt
@@ -1,9 +1,13 @@
+private val puzzleWidth = (0 until 9)
+
 class Puzzle {
-    private val grid = (0 until 9).associateWith { (0 until 9).map { Cell() }.toTypedArray() }
+    private val grid = puzzleWidth.associateWith { puzzleWidth.map { Cell() }.toTypedArray() }
     fun get(x: Int, y: Int) = grid[y]?.let { it[x].value }
     fun set(x: Int, y: Int, value: Int) {
         grid[y]?.let { it[x] = Cell(value) }
     }
+
+    fun rowHas(row: Int, value: Int) = puzzleWidth.any { get(it, row) == value }
 }
 
 data class Cell(var value: Int? = null)
--- src/jvmTest/kotlin/GridTest.kt
+++ src/jvmTest/kotlin/GridTest.kt
@@ -1,5 +1,7 @@
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 class GridTest {
 
@@ -8,5 +10,13 @@ class GridTest {
         val puzzle = Puzzle()
         puzzle.set(0,0, 5)
         assertEquals(5, puzzle.get(0,0))
+    }
+
+    @Test
+    fun rowHas(){
+        val puzzle = Puzzle()
+        puzzle.set(0,0, 5)
+        assertTrue(puzzle.rowHas(0,5))
+        assertFalse(puzzle.rowHas(1,5))
     }
 }
