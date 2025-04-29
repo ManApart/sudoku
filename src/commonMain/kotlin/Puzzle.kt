8736726ade5c4997b572ea1289fd55d050119799
ManApart <r@austinkucera.com>
2025-04-29 07:52:27 -0400

    update possible

--- src/commonMain/kotlin/Puzzle.kt
+++ src/commonMain/kotlin/Puzzle.kt
@@ -2,19 +2,22 @@ private val puzzleWidth = (0 until 9)
 private val gridWidth = (0 until 3)
 
 class Puzzle {
-    private val cells = puzzleWidth.associateWith { puzzleWidth.map { Cell() }.toTypedArray() }
+    private val cells = puzzleWidth.associateWith { y -> puzzleWidth.map { x -> Cell(x, y) }.toTypedArray() }
     private val grids = gridWidth.map { y -> gridWidth.map { x -> buildGrid(x * 3, y * 3, cells) }.toTypedArray() }
 
-    operator fun get(x: Int, y: Int) = cells[y]?.let { it[x].value }
+    operator fun get(x: Int, y: Int) = cells[y]!!.let { it[x] }
     operator fun set(x: Int, y: Int, value: Int) {
         cells[y]?.let { it[x].value = value }
     }
 
-    fun rowHas(row: Int, value: Int) = puzzleWidth.any { get(it, row) == value }
-    fun colHas(col: Int, value: Int) = puzzleWidth.any { get(col, it) == value }
+    fun rowHas(row: Int, value: Int) = puzzleWidth.any { get(it, row).value == value }
+    fun colHas(col: Int, value: Int) = puzzleWidth.any { get(col, it).value == value }
 
     fun grid(gridX: Int, gridY: Int) = grids[gridY].let { it[gridX] }
 
+    fun updatePossible() {
+        cells.values.forEach { row -> row.forEach { it.updatePossible(this) } }
+    }
 }
 
 private fun buildGrid(startX: Int, startY: Int, puzzleCells: Map<Int, Array<Cell>>): Grid {
@@ -26,10 +29,22 @@ private fun buildGrid(startX: Int, startY: Int, puzzleCells: Map<Int, Array<Cell
 data class Grid(private val grid: Map<Int, Array<Cell>>) {
     operator fun get(x: Int, y: Int) = grid[y]?.let { it[x].value }
     fun has(value: Int) = grid.any { row -> row.value.any { it.value == value } }
-//    fun mustHaveInRow(gridX: Int, gridY: Int, gridRow: Int, value: Int) = puzzleWidth.any { get(col, it) == value }
+    fun mustHaveInRow(gridRow: Int, value: Int): Boolean {
+        return false
+//        return !has(value) && grid.entries.filter { (row, _) -> row != gridRow }.none { row -> row.value.any { it.isPossible(value) } }
+    }
 //    fun mustHaveInCol(gridX: Int, gridY: Int, gridCol: Int, value: Int) = puzzleWidth.any { get(col, it) == value }
 }
 
-data class Cell(var value: Int? = null) {
-    private var possibleValues = puzzleWidth.toMutableSet()
+data class Cell(val x: Int, val y: Int, var value: Int? = null) {
+    private val possibleValues = puzzleWidth.toMutableSet()
+    fun isPossible(possible: Int) = value == possible || (value == null && possibleValues.contains(possible))
+
+    fun updatePossible(puzzle: Puzzle) {
+        if (value == null && possibleValues.size > 1) {
+            possibleValues.toList().forEach { possible ->
+                if (puzzle.rowHas(y, possible) || puzzle.colHas(x, possible)) possibleValues.remove(possible)
+            }
+        }
+    }
 }
--- src/jvmTest/kotlin/GridTest.kt
+++ src/jvmTest/kotlin/GridTest.kt
@@ -10,7 +10,7 @@ class GridTest {
     fun basicAccess(){
         val puzzle = Puzzle()
         puzzle[0, 0] = 5
-        assertEquals(5, puzzle[0, 0])
+        assertEquals(5, puzzle[0, 0].value)
     }
 
     @Test
@@ -42,10 +42,24 @@ class GridTest {
         puzzle[4, 2] = 8
         puzzle[5, 2] = 9
 
-        val grid = puzzle.grid(1,0)!!
+        val grid = puzzle.grid(1,0)
         assertEquals(1, grid[0,0])
         assertEquals(3, grid[2,0])
         assertEquals(9, grid[2,2])
+    }
+
+    @Test
+    fun isPossibleRowCol() {
+        val puzzle = Puzzle()
+        puzzle[1, 1] = 5
+        puzzle.updatePossible()
+
+        assertFalse(puzzle[1, 0].isPossible(5))
+        assertFalse(puzzle[1, 2].isPossible(5))
+        assertFalse(puzzle[0, 1].isPossible(5))
+        assertFalse(puzzle[2, 1].isPossible(5))
+        assertFalse(puzzle[1, 1].isPossible(6))
+        assertTrue(puzzle[1, 0].isPossible(6))
     }
 
     @Test
@@ -57,4 +71,20 @@ class GridTest {
         assertFalse(grid.has(6))
         assertFalse(puzzle.grid(1,0).has(5))
     }
+
+//    @Test
+//    fun gridMustHaveInRow(){
+//        val puzzle = Puzzle()
+//        puzzle[0, 0] = 1
+//        puzzle[1, 0] = 2
+//        puzzle[2, 0] = 3
+//
+//        puzzle[0, 2] = 4
+//        puzzle[1, 2] = 5
+//        puzzle[2, 2] = 6
+//
+//        val grid = puzzle.grid(0,0)
+//        assertTrue(grid.mustHaveInRow(1, 7))
+//        assertFalse(grid.mustHaveInRow(0, 7))
+//    }
 }
