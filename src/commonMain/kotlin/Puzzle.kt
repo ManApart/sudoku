9d72fe4ac10e6d62ef1d944918b2bcc170a0fe03
ManApart <r@austinkucera.com>
2025-04-29 07:24:46 -0400

    build grid

--- src/commonMain/kotlin/Puzzle.kt
+++ src/commonMain/kotlin/Puzzle.kt
@@ -1,22 +1,29 @@
 private val puzzleWidth = (0 until 9)
+private val gridWidth = (0 until 3)
 
 class Puzzle {
     private val cells = puzzleWidth.associateWith { puzzleWidth.map { Cell() }.toTypedArray() }
-    //TODO - build grids properly
-    private val grids = puzzleWidth.associateWith { puzzleWidth.map { Grid(mapOf()) }.toTypedArray() }
+    private val grids = gridWidth.map { y -> gridWidth.map { x -> buildGrid(x * 3, y * 3, cells) }.toTypedArray() }
 
     operator fun get(x: Int, y: Int) = cells[y]?.let { it[x].value }
     operator fun set(x: Int, y: Int, value: Int) {
-        cells[y]?.let { it[x] = Cell(value) }
+        cells[y]?.let { it[x].value = value }
     }
 
     fun rowHas(row: Int, value: Int) = puzzleWidth.any { get(it, row) == value }
     fun colHas(col: Int, value: Int) = puzzleWidth.any { get(col, it) == value }
 
     fun grid(gridX: Int, gridY: Int) = grids[gridY]?.let { it[gridX] }
+
 }
 
-data class Grid(private val grid: Map<Int,Array<Cell>>){
+private fun buildGrid(startX: Int, startY: Int, puzzleCells: Map<Int, Array<Cell>>): Grid {
+    return Grid(gridWidth.associateWith { y ->
+        gridWidth.map { x -> puzzleCells[startY + y]!![startX + x] }.toTypedArray()
+    })
+}
+
+data class Grid(private val grid: Map<Int, Array<Cell>>) {
     operator fun get(x: Int, y: Int) = grid[y]?.let { it[x].value }
     //    fun has(gridX: Int, gridY: Int, value: Int) = puzzleWidth.any { get(col, it) == value }
 //    fun mustHaveInRow(gridX: Int, gridY: Int, gridRow: Int, value: Int) = puzzleWidth.any { get(col, it) == value }
