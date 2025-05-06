ecf1806bb4220fc73a63d5e0da685c422724b6bf
ManApart <r@austinkucera.com>
2025-05-06 19:18:38 -0400

    complete puzzle generation

--- src/commonMain/kotlin/PuzzleGenerator.kt
+++ src/commonMain/kotlin/PuzzleGenerator.kt
@@ -4,10 +4,13 @@ fun generatePuzzle(rand: Random): Puzzle {
     val puzzle = importPuzzle(COMPLETE_PUZZLE_TEXT)
     with(puzzle) {
         shuffleNumbers(rand)
-        shuffleRows(rand)
-        if (!isComplete()) println("Failed to generate puzzle!")
+        shuffleGroup(rand, ::swapRows)
+        shuffleGroup(rand, ::swapColumns)
+        shuffleGrid(rand, ::swapRows)
+        shuffleGrid(rand, ::swapColumns)
 
         val isValid = puzzle.cells().none { it.value != null && !puzzle.isValid(it.x, it.y, it.value!!) }
+        if (!isComplete()) println("Failed to generate puzzle!")
         if (!isValid) println("Puzzle is not valid!")
     }
 
@@ -18,31 +21,48 @@ private fun Puzzle.shuffleNumbers(rand: Random) {
     puzzleWidth.forEach { swapNumbers(it + 1, rand.nextInt(8) + 1) }
 }
 
-private fun Puzzle.shuffleRows(rand: Random) {
+private fun shuffleGroup(rand: Random, swapFun: (Int, Int) -> Unit) {
     puzzleWidth.forEach {
         val randomNum = rand.nextInt(3)
         val blockNumber = it / 3
-        swapRows(it, blockNumber * 3 + randomNum)
+        swapFun(it, blockNumber * 3 + randomNum)
     }
 }
 
-private fun Puzzle.swapNumbers(n1: Int, n2: Int) {
+private fun shuffleGrid(rand: Random, swapFun: (Int, Int) -> Unit) {
+    gridWidth.forEach { a ->
+        val b = rand.nextInt(3)
+        gridWidth.forEach {
+            swapFun(a * 3 + it, b * 3 + it)
+        }
+    }
+}
+
+private fun Puzzle.swapNumbers(a: Int, b: Int) {
     puzzleWidth.forEach { y ->
         puzzleWidth.forEach { x ->
             val cell = this[x, y]
-            if (cell.value == n1) {
-                cell.value = n2
-            } else if (cell.value == n2) {
-                cell.value = n1
+            if (cell.value == a) {
+                cell.value = b
+            } else if (cell.value == b) {
+                cell.value = a
             }
         }
     }
 }
 
-private fun Puzzle.swapRows(r1: Int, r2: Int) {
-    val row1 = row(r1)
-    val row2 = row(r2)
+private fun Puzzle.swapRows(a: Int, b: Int) {
+    val row1 = row(a)
+    val row2 = row(b)
     val r1Values = row1.map { it.value }
     row2.forEachIndexed { i, cell -> row1[i].value = cell.value }
     r1Values.forEachIndexed { i, v -> row2[i].value = v }
+}
+
+private fun Puzzle.swapColumns(a: Int, b: Int) {
+    val col1 = col(a)
+    val col2 = col(b)
+    val c1Values = col1.map { it.value }
+    col2.forEachIndexed { i, cell -> col1[i].value = cell.value }
+    c1Values.forEachIndexed { i, v -> col2[i].value = v }
 }
